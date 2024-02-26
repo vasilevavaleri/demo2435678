7261c17d2ced79cdf902e34fac4b690d3ef1fca1
vasilevavaleri <153999612+vasilevavaleri@users.noreply.github.com>
2024-02-27 01:13:15 +0600

    Delete demo directory

--- demo/src/main/java/com/example/demo/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-	List<Product> findByTitle(String title);
-}
